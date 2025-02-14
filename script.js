ddf1fee5c7707c36109aaa5d4c67a2dfeb8299d4
Davut Simsek <davutt.simsek@gmail.com>
2025-02-14 15:23:15 +0100

    implement displaying a map marker

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+///////////////////////////////////////////////////////////////////////////////////////
+/////   Implement Rendering Map on Current Location, Displaying Map Marker   //////////
+///////////////////////////////////////////////////////////////////////////////////////
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
     function (position) {
@@ -18,23 +21,43 @@ if (navigator.geolocation)
       const { longitude } = position.coords;
       console.log(position);
       console.log(latitude, longitude);
+
       console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
       const coords = [latitude, longitude];
 
       const map = L.map('map').setView(coords, 13);
 
+      //   console.log(map);
+
       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-        .openPopup();
+      map.on(`click`, function (mapEvent) {
+        console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 150,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent(`Workout`)
+          .openPopup();
+      });
     },
     function () {
       alert(`Could not get your position`);
     }
   );
+
+///////////////////////////////////////////////////////////////////////////////////////
+////////////////              Rendering Workout Input Form                   //////////
+///////////////////////////////////////////////////////////////////////////////////////
