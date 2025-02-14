f923d5b2ae990b6cccfa36f6912ce22b2db9b4aa
Davut Simsek <davutt.simsek@gmail.com>
2025-02-14 16:10:36 +0100

    implement onClick get workout form

--- script.js
+++ script.js
@@ -3,6 +3,10 @@
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
+///////////////////////////////////////////////////////////////////////////////////////
+/////                                ELEMENTS                                //////////
+///////////////////////////////////////////////////////////////////////////////////////
+
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
 const inputType = document.querySelector('.form__input--type');
@@ -11,22 +15,24 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
+
 ///////////////////////////////////////////////////////////////////////////////////////
 /////   Implement Rendering Map on Current Location, Displaying Map Marker   //////////
 ///////////////////////////////////////////////////////////////////////////////////////
+
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
     function (position) {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
-      console.log(position);
-      console.log(latitude, longitude);
-
-      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+      //   console.log(position);
+      //   console.log(latitude, longitude);
+      //   console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
       const coords = [latitude, longitude];
 
-      const map = L.map('map').setView(coords, 13);
+      map = L.map('map').setView(coords, 13);
 
       //   console.log(map);
 
@@ -35,22 +41,11 @@ if (navigator.geolocation)
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      map.on(`click`, function (mapEvent) {
-        console.log(mapEvent);
-        const { lat, lng } = mapEvent.latlng;
-        L.marker([lat, lng])
-          .addTo(map)
-          .bindPopup(
-            L.popup({
-              maxWidth: 250,
-              minWidth: 150,
-              autoClose: false,
-              closeOnClick: false,
-              className: 'running-popup',
-            })
-          )
-          .setPopupContent(`Workout`)
-          .openPopup();
+      // Handling clicks on map
+      map.on(`click`, function (mapE) {
+        mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
       });
     },
     function () {
@@ -61,3 +56,37 @@ if (navigator.geolocation)
 ///////////////////////////////////////////////////////////////////////////////////////
 ////////////////              Rendering Workout Input Form                   //////////
 ///////////////////////////////////////////////////////////////////////////////////////
+
+form.addEventListener(`submit`, function (e) {
+  e.preventDefault();
+
+  // Clear input fields
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      '';
+
+  // Displaying marker
+  console.log(mapEvent);
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 150,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent(`Workout`)
+    .openPopup();
+});
+
+// Get Cadence for Running, Elevation for Cycling
+inputType.addEventListener(`change`, function () {
+  inputElevation.closest(`.form__row`).classList.toggle(`form__row--hidden`);
+  inputCadence.closest(`.form__row`).classList.toggle(`form__row--hidden`);
+});
